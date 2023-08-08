774036c05d2fa614e257b953ba56e9dfe5186299
Jason Eckstein <jekyllstein@gmail.com>
2023-08-07 18:24:01 -0600

    Add PlutoStartup module

---
+++ PlutoStartup/src/PlutoStartup.jl
@@ -0,0 +1,18 @@
+module PlutoStartup
+
+using Reexport, PrecompileTools, Base.Threads
+
+@reexport using Pluto, PlutoSliderServer, Statistics, StatsBase
+
+function __init__()
+    Pluto.run()
+end
+# @setup_workload begin
+#     using PlotlyJS
+#     @compile_workload begin
+#         t = scatter(x = rand(100), y = rand(100); mode = "markers")
+#         p = plot(t)
+#         savefig(p, "test.html")
+#     end
+# end
+end # module PlutoStartup
