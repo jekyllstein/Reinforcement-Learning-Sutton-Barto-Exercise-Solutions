a629ac9423c236262e595d934f056067492ee82b
Jason Eckstein <jekyllstein@gmail.com>
2024-10-16 13:01:41 -0600

    Add Chapter 10 to NonTabularRL.jl

--- NonTabularRL.jl/src/NonTabularRL.jl
+++ NonTabularRL.jl/src/NonTabularRL.jl
@@ -10,7 +10,9 @@ include(joinpath(@__DIR__, "..", "..", "Chapter-9", "Chapter_9_On-policy_Predict
 
 export gradient_monte_carlo_episode_update!, gradient_monte_carlo_policy_estimation!, gradient_monte_carlo_estimation!, semi_gradient_td0_update!, semi_gradient_td0_policy_estimation!, semi_gradient_td0_estimation!, semi_gradient_td0_policy_estimation, semi_gradient_td0_estimation, make_random_walk_mrp, state_aggregation_gradient_setup, run_state_aggregation_monte_carlo_policy_estimation, run_state_aggregation_semi_gradient_policy_estimation, order_features_gradient_setup, run_order_features_monte_carlo_policy_estimation, calc_poly_feature, calc_fourier_feature, tile_coding_gradient_setup, run_tile_coding_monte_carlo_policy_estimation
 
-# include(joinpath(@__DIR__, "..", "..", "Chapter-10", "On-policy Control with Approximation.jl"))
+include(joinpath(@__DIR__, "..", "..", "Chapter-10", "Chapter_10_On_policy_Control_with_Approximation.jl"))
+
+export run_fcann_semi_gradient_sarsa
 
 @setup_workload begin
     γ = 0.9f0
